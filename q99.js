7490e47471f9ea1bca0b7e8f620852336e3a151d
danish-noor <kinzadani00@gmail.com>
2024-04-03 22:35:10 +0500

    100DaysCoding

---
+++ q99.js
@@ -0,0 +1,13 @@
+//Question 99: Generate a date object representing your next birthday and log it to the console.
+function getNextBirthday(month, day) {
+    const currentDay = new Date();
+    let year = currentDay.getFullYear();
+    const birthday = new Date(year, month - 1, day);
+    if (birthday < currentDay) {
+        birthday.setFullYear(year + 1);
+    }
+    return birthday;
+}
+const nextBirthday = getNextBirthday(3, 29);
+console.log("Next birthday on:", nextBirthday.toLocaleDateString());
+export {};
