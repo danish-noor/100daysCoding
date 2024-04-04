4ba762aa11acf2801e59dbd58223720e5a3dc97d
danish-noor <kinzadani00@gmail.com>
2024-04-04 18:45:47 +0500

    100DaysCoding

---
+++ q100.ts
@@ -0,0 +1,6 @@
+//Question 100: Use the JavaScript Math object to find the square root of 144.
+  
+  const squareRoot = Math.sqrt(144);
+
+  console.log(`The square root of 144 is ${squareRoot}`);
+  
---
+++ q101.ts
@@ -0,0 +1,7 @@
+//Question 101: Generate a random integer between 1 and 10.
+
+function randomInteger(): number{
+    return Math.floor(Math.random() * 10) +1
+
+}
+console.log(randomInteger());
