e6d59f07380ad5417edf0bff12659050129cf4a3
danish-noor <kinzadani00@gmail.com>
2024-04-12 16:22:00 +0500

    100DaysCoding

---
+++ q123.ts
@@ -0,0 +1,20 @@
+//Question 123: Create a loop that iterates through a string and stops when it finds the first vowel.
+
+function findFirstVovel(str:string){
+    const vovel ="aeiouAEIOU";
+
+    for( const char of str) {
+        if (vovel.includes(char)){
+            console.log(`First vovel found:${char}`);
+            break;
+        }
+        console.log(char);
+    }
+        
+    }
+    
+    findFirstVovel("pakistan"); // Logs 'p', 'a', 'k', 'i', 's','t','a','n',and then 'First vowel found: a'
+    // Demonstrates iterating through a string and stopping at the first vowel.
+
+
+
